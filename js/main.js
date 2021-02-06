59d8e639081ba389f65e070e2292d94e1771c28e
Koladele Olaitan <koladeleolaitan@gmail.com>
2021-02-06 13:43:00 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -8,28 +8,46 @@ navBtn.addEventListener('click', ()=> {
     navBtn.classList.toggle('active');
 });
 
-let i = 0;
-const blogCont = document.querySelector('.blog-cont');
-const posts = document.querySelectorAll('.xtra-post');
-const footer = document.querySelector('footer').offsetHeight;
 
-blogCont.onscroll = ()=> {
-    if (blogCont.scrollTop === post[0].offsetHeight * 5 ){
-        posts.forEach(post => {
-            post.classList.remove('d-none');
-            console.log('works');
-        });
-    } else {
-        posts.forEach(post => {
-            post.classList.add('d-none');
-        });
+//fixed header 
+const header = document.querySelector('header');
+const navbarList = document.querySelectorAll('nav a');
+const lp = document.querySelector('.landing-page');
+
+const slideObserverOptions = {};
+const slideObserver = new IntersectionObserver((entries, slideObserver) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            header.classList.add('fixed');
+            navbarList.forEach(listItem => {
+                listItem.classList.add('fixed');
+            });
+        } else {
+            header.classList.remove('fixed');
+            navbarList.forEach(listItem => {
+                listItem.classList.remove('fixed');
+            });
+        }
+    });
+}, slideObserverOptions);
+
+slideObserver.observe(lp);
+
+
+
+//slider
+const slider = document.querySelector('.slider');
+const slide = document.querySelectorAll('.slide');
+let counter = 1;
+
+showSlides();
+function showSlides() {
+    slider.style.transform = `translateX(${-counter * 25}%)`;
+    slider.style.transition = `transform 0.4s ease-in-out`;
+    counter++;
+    if (counter === slide.length) {
+        counter = 1;
     }
-};
 
-// const showPosts = (extraPosts, posts)=> {
-//     if (document.body.scrollTop > posts[0].offsetHeight * 5 + lp.offsetHeight){
-//         for(i = 0; i >= 5; i++) {
-//             posts[i].style.display = "block";
-//         }
-//     }
-// };
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
